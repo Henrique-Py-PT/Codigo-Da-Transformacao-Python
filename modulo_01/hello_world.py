cb20ab35ac5e3598093917b308eed805e6c40489
Victor souza romano <fernandes9132qi@gmail.com>
2025-10-27 14:14:58 -0300

    atividades ead

---
+++ modulo_01/hello_world.py
@@ -0,0 +1,20 @@
+'''
+nomeclaturas:
+PASCALCase
+CamelCase
+Snake_Case
+kebab-Case
+
+comentarios, pode ser usados como apostofos
+ou corquilhas
+
+
+
+
+
+'''
+
+# Exibe a mensagem na tela
+print("hello, world!")
+
+input("muito bem, qual seu nome?")
